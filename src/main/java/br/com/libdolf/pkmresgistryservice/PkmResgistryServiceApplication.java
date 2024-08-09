5e8168e1a5870f4758ad2a7aa8562faafc17eb6f
Lindolfo Moizinho <lindolfomoizinho1@gmail.com>
2024-08-09 19:42:17 -0300

    First Commit

---
+++ src/main/java/br/com/libdolf/pkmresgistryservice/PkmResgistryServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.libdolf.pkmresgistryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkmResgistryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PkmResgistryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/libdolf/pkmresgistryservice/PkmResgistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.libdolf.pkmresgistryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkmResgistryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
